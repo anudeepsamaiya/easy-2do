0c2ab2fefc5a257deaaa6b0a0c667d854cb6d105
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-08-09 17:18:41 +0530

    added migrations for tasks app

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import User
 from django.db import models
+from usermanagement.models import User
 
 class TaskStatus(models.Model):
     code = models.IntegerField()
@@ -8,10 +8,15 @@ class TaskStatus(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        db_table = 'TaskStatus'
+        verbose_name = 'TaskStatus'
+        verbose_name_plural = 'TaskStatuses'
+
 
 class Tasks(models.Model):
     STATUS_TODO = 0
-    STATUS_INPROGRESS = 1
+    STATUS_IN_PROGRESS = 1
     STATUS_DONE = 2
 
     reference_id = models.CharField(unique=True, max_length=13)
@@ -26,3 +31,8 @@ class Tasks(models.Model):
 
     def __str__(self):
         return ' '.join([self.reference_id, self.name])
+
+    class Meta:
+        db_table = 'Task'
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
