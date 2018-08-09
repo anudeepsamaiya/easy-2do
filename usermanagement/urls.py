e8741714ff88418010303fb355f7524404d2b366
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-08-09 17:21:20 +0530

    add serializer for usermanagement

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register(r'tasks', TaskViewSet, base_name='task')
+router.register(r'users', UserViewSet, base_name='user')
 
 urlpatterns = router.urls
 
--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from usermanagement.models import User
+from usermanagement.serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer()
+
+    def create(self, *args, **kwargs):
+        return super(UserViewSet, self).create(*args, **kwargs)
+
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
