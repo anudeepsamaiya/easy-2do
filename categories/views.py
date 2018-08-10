1878a477770997789bdd009e6a4948e6298a2b26
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-08-11 00:16:40 +0530

    add filters for category and subcategory

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
 
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Category, SubCategory
 from .serializers import CategorySerializer, SubCategorySerializer
@@ -8,8 +9,13 @@ from .serializers import CategorySerializer, SubCategorySerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('code', 'name',
+            'sub_categories__name', 'sub_categories__code')
 
 
 class SubCategoryViewSet(viewsets.ModelViewSet):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('code', 'name', 'category__code', 'category__name')
