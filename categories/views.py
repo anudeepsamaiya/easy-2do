62d8f491101ba7a486249360b0115a45ef9f79cb
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-08-11 00:17:43 +0530

    use select_related and prefetch related

--- categories/views.py
+++ categories/views.py
@@ -7,7 +7,7 @@ from .serializers import CategorySerializer, SubCategorySerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
+    queryset = Category.objects.prefetch_related('sub_categories')
     serializer_class = CategorySerializer
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ('code', 'name',
@@ -15,7 +15,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class SubCategoryViewSet(viewsets.ModelViewSet):
-    queryset = SubCategory.objects.all()
+    queryset = SubCategory.objects.select_related('category')
     serializer_class = SubCategorySerializer
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ('code', 'name', 'category__code', 'category__name')
