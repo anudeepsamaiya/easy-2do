00a7ca95773f2c5898e1ac179f77f3cdb5a26af9
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-08-21 12:39:02 +0530

    Create an abstract model AbstractTask and modify Task model as concrete for this model

--- tasks/models.py
+++ tasks/models.py
@@ -15,23 +15,29 @@ class TaskStatus(models.Model):
         verbose_name_plural = 'TaskStatuses'
 
 
-class Tasks(models.Model):
-    STATUS_TODO = 0
+class AbstractTask(model_extensions.TitleSlugDescriptionModel,
+        model_extensions.TimeStampedModel, model_extensions.ActivatorModel):
+
+    STATUS_PENDING = 0
     STATUS_IN_PROGRESS = 1
     STATUS_DONE = 2
 
-    reference_id = models.CharField(unique=True, max_length=13)
-    name = models.CharField(max_length=80)
-    description = models.TextField(max_length=250, null=True, blank=True)
+    ref_id = models.CharField(unique=True, max_length=13)
+    task_status = models.ForeignKey(TaskStatus, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return ' : '.join([self.ref_id, self.slug])
+
+    class Meta:
+        abstract = True
+
+
+class Task(AbstractTask):
     reporter = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
             related_name='reported_tasks')
     assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
             related_name='assigned_tasks')
-    status = models.ForeignKey(TaskStatus, on_delete=models.SET_DEFAULT,
-            default=0)
-
-    def __str__(self):
-        return ' '.join([self.reference_id, self.name])
+    due_date = models.DateTimeField(null=True)
 
     class Meta:
         db_table = 'Task'
