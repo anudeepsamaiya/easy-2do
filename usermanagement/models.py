14541ef4e84f24da7c8a39d7ef34e08786f44eea
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-08-21 12:05:47 +0530

    rename users app to usermanagement

---
+++ usermanagement/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+#  from django.contrib.auth.models import (
+#      AbstractUser, BaseUserManager
+#  )
+#
+#  # Create your models here.
+#
+#  class User(AbstractUser):
+#      identifier = models.CharField(unique=True, max_length=10)
+#
+    #  USERNAME_FIELD = 'identifier'
