111ce524c8bab1bd95d1cc7b53adc58c49bfd9c7
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-08-21 13:36:38 +0530

    update task serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,9 +1,23 @@
 
 from rest_framework import serializers
-from tasks.models import Task
+from .models import Task
 
 
 class TaskSerializer(serializers.Serializer):
+    ref_id = serializers.CharField(read_only=True)
+    title = serializers.CharField()
+    description = serializers.CharField()
+    task_status = serializers.PrimaryKeyRelatedField(queryset=TaskStatus.objects.all())
+    parent = serializers.PrimaryKeyRelatedField(
+            queryset=Task.objects.filter(parent__isnull=True), allow_null=True)
+
     class Meta:
         model = Task
         fields = []
+
+    def update(self, instance, validated_data):
+        instance.save()
+        return instance
+
+    def create(self, validated_data):
+        return Task.objects.create(**validated_data)
